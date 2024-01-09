99f86f5443dc6bf524b70ca1075d10b14b220a92
Mykyta <mykytamyrhorod@gmail.com>
2024-01-09 08:55:00 +0100

    proverka

---
+++ js/main.js
@@ -0,0 +1,10 @@
+new Swiper ('.swiper-container',{
+    navigation: {
+        nextEl: '.swiper-button-next-2',
+        prevEl: '.swiper-button-prev-1'
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
